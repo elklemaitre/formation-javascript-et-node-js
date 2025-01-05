9440f2f6b976da910ef8e6a15d3eaeb41f041368
elklemaitre <153718154+elklemaitre@users.noreply.github.com>
2025-01-05 22:25:35 +0100

    Add files via upload

---
+++ TP_Inventory_JavaSwing/src/fr/tpjava/Main.java
@@ -0,0 +1,11 @@
+package fr.tpjava;
+import fr.tpjava.Inventory;
+import fr.tpjava.GameInventory;
+
+public class Main {
+    public static void main(String[] args) {
+        Inventory items = new Inventory();
+
+        new GameInventory();
+    }
+}
